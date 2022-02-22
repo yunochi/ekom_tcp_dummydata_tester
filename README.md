b86000b5dccbeaaec504489ebc9021d7b1c22de9
Yuri Lee <yuno@yunochi.com>
2022-02-22 18:10:48 +0900

    첫 버전

---
+++ README.md
@@ -0,0 +1,10 @@
+#ekom 서버 테스트용 더미 TCP 클라이언트 프로그램
+
+
+### 1. 설정법  
+1.1 config.ini 파일을 편집합니다.  
+1.2 _server_hostname_ 에 서버 ip, 혹은 주소를 입력합니다.  
+1.3 _server_port_ 에 서버의 포트를 입력합니다.  
+1.4 _send_interval_ 에는 데미 데이터 전송 간격을 설정합니다.  
+### 2. 실행  
+2.1 python3 를 이용하여 main.py 를 실행합니다. (_config.ini_ 파일은 같은 디렉터리에 존재해야 합니다)
---
+++ main.py
@@ -0,0 +1,68 @@
+import socket
+import configparser
+import time
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+server_hostname = config.get('settings', 'server_hostname')
+server_port = int(config.get('settings', 'server_port'))
+send_interval = float(config.get('settings', 'send_interval'))
+
+
+def init_tcp_socket():
+    global client_sock
+    print('socket_init: TCP socket init...')
+    try:
+        client_sock.settimeout(5)
+        client_sock.connect((server_hostname, server_port))
+
+    except Exception as err:
+        print('socket_init: TCP sock connect error!', err)
+        raise SystemExit
+    else:
+        return True
+
+
+def send_data_tcp_sock(payload_bytes):
+    try:
+        if client_sock.send(payload_bytes) == len(payload_bytes):
+            return True
+        else:
+            print('tcp_send: TCP send fail!')
+            return False
+    except Exception as err:
+        print('tcp_send: TCP send exception:', err)
+        return False
+
+
+if __name__ == '__main__':
+    client_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    init_tcp_socket()
+    device_serial = '12311'
+    in_flowrate = '122'
+    in_temperature = '+12.3'
+    in_humidity = '71'
+    outside_temperature = '-12.3'
+    outside_humidity = '54'
+    ventilation_temperature = '+23.2'
+    ventilation_humidity = '42'
+    co2_value = '491'
+    filter_differential_pressure = '233'
+
+    tx_counter = 0
+
+    while True:
+        tx_str = '{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(device_serial, in_flowrate, in_temperature, in_humidity,
+                                                       outside_temperature, outside_humidity, ventilation_temperature,
+                                                       ventilation_humidity, co2_value, filter_differential_pressure)
+        print('DEBUG: tx_str:{}'.format(tx_str))
+        tx_byte = tx_str.encode(encoding='utf-8', errors='ignore')
+        if not send_data_tcp_sock(tx_byte):
+            # TCP socket send error
+            print('TCP send error, try init tcp socket')
+            init_tcp_socket()
+
+        tx_counter = tx_counter + 1
+        print('DEBUG: TX_counter:{}'.format(tx_counter))
+        time.sleep(send_interval)
